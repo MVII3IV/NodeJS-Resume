ca560831d5e41e7cf495081440b668f278b6c0ee
mvii3iv <afrosclothing@hotmail.com>
2017-06-02 16:28:42 -0600

    json data was added

--- app.js
+++ app.js
@@ -12,8 +12,55 @@ app.engine('.hbs', handlebars({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 
 
+var user = {
+    "name": "Erick Dom",
+    "title": "Developer",
+    "description": "Software developer offering four years of experience in the full software development cycle, whose forte is backend utilizing both java and spring framework, with knowledge in frontend using bootstrap and angularjs as well as in graphic design, l have spend the last two years developing internal web application to control and send orders remotely to more than 80 machines located in memphis, constantly in touch with customers located in california.",
+    "personalInfo": 
+        {
+            "email":"erickdom@outlook.com", 
+            "phone": "614-132-2363", 
+            "website": "erickdom.com"
+        },
+    "carrer":
+        [
+            
+             {
+                "jobTitle":"Programmer", 
+                "period": "2015 - Present", 
+                "company": "Autozone - Alldata",
+                "location": "Chihuahua MX.",
+                "jobDescrition" : ""
+            },
+            {
+                "jobTitle":"Associate Programmer", 
+                "period": "2013 - 2015", 
+                "company": "Autozone - Alldata",
+                "location": "Chihuahua MX.",
+                "jobDescrition" : ""
+            },
+            {
+                "jobTitle":"Intern", 
+                "period": "2012 - 2013", 
+                "company": "Autozone - Alldata",
+                "location": "Chihuahua MX.",
+                "jobDescrition" : ""
+            }
+           
+
+        ],
+        "professionalSkills" :
+        [
+            { "language": "Java", "experience": "4 Years", "percent": 80 },
+            { "language": "C#", "experience": "4 Years", "percent": 70 },
+            { "language": "Javascript", "experience": "2 Years","percent": 60 }
+        ],
+        "otherSkills" : ["HTML5", "CSS3", "Git", "Angular", "Spring"]
+};
+
+
 app.get("/", function(req, res){
-    res.render('index', {list: 'lest see'});
+    res.render('index', {'user': user});
 });
 
 
